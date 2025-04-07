cecb300b6a7195e7afd7adbce498853990e9f2d8
rishu <freeloadermr@gmail.com>
2025-04-07 07:22:34 +0530

    modified script to generate more gpt generated samples and trained model with accuracy between 60-70 for intent prediction

--- model/intent_classifier/modelling.py
+++ model/intent_classifier/modelling.py
@@ -5,11 +5,12 @@ import numpy as np
 import tensorflow as tf
 import tf2onnx
 from sklearn.preprocessing import LabelEncoder
-from transformers import TFBertModel, BertTokenizer
-from tensorflow.keras.layers import Input, Dropout, Dense, Lambda # type: ignore
-from tensorflow.keras.models import Model # type: ignore
-from tensorflow.keras.optimizers import Adam # type: ignore
-from tensorflow.keras.losses import SparseCategoricalCrossentropy # type: ignore
+from transformers import TFBertModel, BertTokenizer, BertConfig
+from tensorflow.keras.layers import Input, Dropout, Dense, Lambda  # type: ignore
+from tensorflow.keras.models import Model  # type: ignore
+from tensorflow.keras.optimizers import Adam  # type: ignore
+from tensorflow.keras.losses import SparseCategoricalCrossentropy  # type: ignore
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from preprocessing_data import load_and_tokenize_data
 
@@ -17,17 +18,20 @@ MODEL_NAME = 'bert-base-uncased'
 MODEL_PATH = 'model/intent_classifier/model.keras'
 ENCODER_PATH = 'model/intent_classifier/label_encoder.pkl'
 
+
 def build_model(num_labels):
-    bert = TFBertModel.from_pretrained(MODEL_NAME)
+    config = BertConfig.from_pretrained(MODEL_NAME)
+    config.hidden_act = "gelu_new"
+
+    bert = TFBertModel.from_pretrained(MODEL_NAME, config=config)
 
     input_ids = Input(shape=(64,), dtype=tf.int32, name="input_ids")
     attention_mask = Input(shape=(64,), dtype=tf.int32, name="attention_mask")
 
-    bert_output = Lambda(lambda x: bert(x[0], attention_mask=x[1]).last_hidden_state[:, 0, :], output_shape=(768,))(
-        [input_ids, attention_mask]
-    )
-    # outputs = bert(input_ids, attention_mask=attention_mask)
-    # cls_output = outputs.last_hidden_state[:, 0, :] 
+    bert_output = Lambda(
+        lambda x: bert(x[0], attention_mask=x[1]).last_hidden_state[:, 0, :],
+        output_shape=(768,)
+    )([input_ids, attention_mask])
 
     dropout = Dropout(0.3)(bert_output)
     output = Dense(num_labels, activation='softmax')(dropout)
@@ -35,8 +39,8 @@ def build_model(num_labels):
     model = Model(inputs=[input_ids, attention_mask], outputs=output)
 
     model.compile(
-        optimizer=Adam(learning_rate=1e-4), 
-        loss=SparseCategoricalCrossentropy(from_logits=False), 
+        optimizer=Adam(learning_rate=1e-4),
+        loss=SparseCategoricalCrossentropy(from_logits=False),
         metrics=['accuracy']
     )
 
@@ -49,21 +53,21 @@ if __name__ == '__main__':
     model = build_model(num_labels=len(label_encoder.classes_))
 
     # Train the model
-    model.fit(dataset.batch(8), epochs=5)
+    model.fit(dataset.batch(8), epochs=25)
 
     # Save the trained model
     model.save(MODEL_PATH)
 
-    # Save the label encoder using joblib
+    # Save the label encoder
     joblib.dump(label_encoder, ENCODER_PATH)
 
-    # Save model as ONNX
+    # Export to ONNX
     spec = (
         tf.TensorSpec((None, 64), tf.int32, name="input_ids"),
         tf.TensorSpec((None, 64), tf.int32, name="attention_mask")
-    ) 
+    )
 
-    onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=[spec])
+    onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=[spec], opset=13)
 
     onnx_path = "model/intent_classifier/model.onnx"
     with open(onnx_path, "wb") as f:
